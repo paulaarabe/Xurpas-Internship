17998cab789bbe3e1f3febc00d1f3e5b0a35fabb
MarkMurasaki <mark.murasaki.chan@gmail.com>
2023-04-20 16:54:45 +0800

    Subject Module

--- src/modules/subject/controller/subject.controller.ts
+++ src/modules/subject/controller/subject.controller.ts
@@ -1,21 +1,21 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { SubjectService } from '../service/subject.service';
-import { CreateSubjectDto } from '../dto/input/create-subject.input';
-import { UpdateSubjectDto } from '../dto/input/update-subject.input';
+import { CreateSubjectInput } from '../dto/input/create-subject.input';
+import { UpdateSubjectInput } from '../dto/input/update-subject.input';
 
 @Controller('subject')
 export class SubjectController {
   constructor(private readonly subjectService: SubjectService) {}
 
   @Post()
-  create(@Body() createSubjectDto: CreateSubjectDto) {
+  create(@Body() createSubjectDto: CreateSubjectInput) {
     return this.subjectService.create(createSubjectDto);
   }
 
-  @Get()
-  findAll() {
-    return this.subjectService.findAll();
-  }
+  // @Get()
+  // findAll() {
+  //   return this.subjectService.findAll();
+  // }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
@@ -23,7 +23,7 @@ export class SubjectController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSubjectDto: UpdateSubjectDto) {
+  update(@Param('id') id: string, @Body() updateSubjectDto: UpdateSubjectInput) {
     return this.subjectService.update(+id, updateSubjectDto);
   }
 
