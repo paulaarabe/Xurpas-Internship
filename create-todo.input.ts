63c7570608e6987e93dd48fb466dd136239f375f
Paula Arabe <116294803+paulaarabe@users.noreply.github.com>
2023-04-21 18:42:18 +0800

    create-todo.input.ts
    typo corrected

---
+++ create-todo.input.ts
@@ -0,0 +1,22 @@
+import { Field, InputType } from '@nestjs/graphql';
+
+@InputType()
+export class CreateTodoInput {
+  @Field()
+  title: string;
+
+  @Field()
+  description: string;
+
+  @Field()
+  dateCreated: Date;
+
+  @Field({ nullable: true })
+  dateUpdated: Date;
+
+  @Field({ nullable: true })
+  dueDate: Date;
+
+  @Field()
+  isCompleted: boolean;
+}
