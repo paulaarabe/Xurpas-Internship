ba2f16e804770aca4df5990e8a148fa14760cda1
MarkMurasaki <mark.murasaki.chan@gmail.com>
2023-04-25 15:46:13 +0800

    Testing GraphQL

--- src/modules/subject/subject.module.ts
+++ src/modules/subject/subject.module.ts
@@ -3,11 +3,12 @@ import { SubjectEntity } from '@entities/subject.entity';
 import { SubjectService } from './service/subject.service';
 import { SubjectController } from './controller/subject.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SubjectRepository } from './repository/subject.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([SubjectEntity])],
   controllers: [SubjectController],
-  providers: [SubjectService]
+  providers: [SubjectService,SubjectRepository],
 })
 export class SubjectModule {}
 
